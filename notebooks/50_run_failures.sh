ac89a5643f9179c8ad320622e0e3e1abca3b8060
gaurav-hu <gaurav.ganti@gmail.com>
2024-03-17 17:19:55 +0000

    Minor update run failures

--- notebooks/50_run_failures.sh
+++ notebooks/50_run_failures.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 #conda activate cdr_climate_uncertainty
 
-while IFS=, read -r number model scenario i; 
+echo "Running re-run batches"
+while IFS=, read -r index i model scenario; 
 do
     echo "Running iterations for ${model} ${scenario} ${i}"
-    papermill 403_iterate_for_cdr.ipynb -p ENSEMBLE_MEMBER $i -p MODEL "$model" -p SCENARIO "$scenario" papermill_output/"403_${i}_${model}_${scenario}.ipynb"
-done < ../data/501_rerun.csv
-
-echo "Running batch 2"
-while IFS=, read -r number model scenario i; 
-do
-    echo "Running iterations for ${model} ${scenario} ${i}"
-    papermill 403_iterate_for_cdr.ipynb -p ENSEMBLE_MEMBER $i -p MODEL "$model" -p SCENARIO "$scenario" papermill_output/"403_${i}_${model}_${scenario}.ipynb"
-done < ../data/502_rerun_batch2.csv
+    papermill 403_iterate_for_cdr.ipynb -p ENSEMBLE_MEMBER $i -p MODEL "$model" -p SCENARIO "$scenario" papermill_output/"403_${i}_aim_${scenario}.ipynb"
+done < ../data/405_final.csv
